e84897bb649b3f98909f76c52b371f59cbdbaa2f
lelong534 <lelong220798@gmail.com>
2020-11-08 22:57:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import './sb-admin-2.css'
+import SideBar from './components/SideBar'
+import NavBar from './components/NavBar'
+import Page from './containers/Page'
+import DashBoard from './containers/DashBoard'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<Router>
+		<div id="wrapper">
+			<SideBar />
+
+		    <div id="content-wrapper" className="d-flex flex-column">
+
+		      <div id="content">
+
+		        <NavBar />
+
+		        <div className="container-fluid">
+
+		          	<div className="d-sm-flex align-items-center justify-content-between mb-4">
+			        	<Switch>
+			        		<Route path="/" exact component={DashBoard} />
+			        		<Route path="/page" component={Page} />
+			        	</Switch>
+			        </div>
+			      </div>
+			    </div>
+			</div>
+		</div>
+	</Router>
   );
 }
 
---
+++ src/containers/DashBoard.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+import styled from 'styled-components'
+
+export default function DashBoard() {
+	return (
+		<div>
+			Dashboard
+		</div>
+	)
+}
